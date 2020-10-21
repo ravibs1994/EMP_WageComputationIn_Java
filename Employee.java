d3edbc87c66e9e15e16569d34f78a1d851fb4c7b
unknown <ravibs1994@gmail.com>
2020-10-21 16:24:39 +0530

    Monthly Wages

--- Employee.java
+++ Employee.java
@@ -7,15 +7,18 @@ public class Employee {
 		int FULL_DAY_HOUR = 8;
 		int PART_TIME_DAY_HOUR = 4;
 		int totalWages;
+		int DAYS_IN_MONTH=20;
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		switch (empCheck) {
 		case 1:
 			totalWages = WAGES_PER_HOUR * FULL_DAY_HOUR;
 			System.out.println("Total Daily Wages = " + totalWages);
+			System.out.println("Total Monthly Wages = "+totalWages*DAYS_IN_MONTH);
 			break;
 		case 2:
 			totalWages = WAGES_PER_HOUR * PART_TIME_DAY_HOUR;
 			System.out.println("Total Daily Wages = " + totalWages);
+			System.out.println("Total Monthly Wages = "+totalWages*DAYS_IN_MONTH);
 			break;
 		default:
 			System.out.println("Employee is Absent ");
