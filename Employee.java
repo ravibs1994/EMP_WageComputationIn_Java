b05dce576868070eaf8e826a9d0ede4d16a72fd4
unknown <ravibs1994@gmail.com>
2020-10-21 15:48:13 +0530

    Part Time Daily Wages

--- Employee.java
+++ Employee.java
@@ -5,15 +5,20 @@ public class Employee {
 	public static void main(String[] args) {
 
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int WAGES_PER_HOUR = 20;
 		int FULL_DAY_HOUR = 8;
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		int PART_TIME_DAY_HOUR = 4;
+		int totalWages;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_FULL_TIME) {
-			int total_Wages = WAGES_PER_HOUR * FULL_DAY_HOUR;
-			System.out.println("Total Daily Wages = " + total_Wages);
+			totalWages = WAGES_PER_HOUR * FULL_DAY_HOUR;
+			System.out.println("Total Daily Wages = " + totalWages);
+		} else if (empCheck == IS_PART_TIME) {
+			totalWages = WAGES_PER_HOUR * PART_TIME_DAY_HOUR;
+			System.out.println("Total Daily Wages = " + totalWages);
 		} else {
-			System.out.println("Employee Is Absent ");
-
+			System.out.println("Employee is Absent ");
 		}
 
 	}
