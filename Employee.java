b436b8947e4cd6e3d0ee5ee702a955fd40395a54
unknown <ravibs1994@gmail.com>
2020-10-21 14:27:27 +0530

    Check_EmpPresentorAbsent

--- Employee.java
+++ Employee.java
@@ -3,6 +3,12 @@ package com.brigelabz;
 public class Employee {
 
 	public static void main(String[] args) {
-		System.out.println(" Welcome to Employee Wage");
+		int IS_FULL_TIME = 1;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
