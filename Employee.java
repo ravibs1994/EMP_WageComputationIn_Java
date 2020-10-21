bceb760b2e8e63b53254920bedea2777921c610a
unknown <ravibs1994@gmail.com>
2020-10-21 14:58:29 +0530

    Calculate Daily Wages

--- Employee.java
+++ Employee.java
@@ -3,12 +3,18 @@ package com.brigelabz;
 public class Employee {
 
 	public static void main(String[] args) {
+
 		int IS_FULL_TIME = 1;
+		int WAGES_PER_HOUR = 20;
+		int FULL_DAY_HOUR = 8;
 		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is present");
+			int total_Wages = WAGES_PER_HOUR * FULL_DAY_HOUR;
+			System.out.println("Total Daily Wages = " + total_Wages);
 		} else {
-			System.out.println("Employee is Absent");
+			System.out.println("Employee Is Absent ");
+
 		}
+
 	}
 }
