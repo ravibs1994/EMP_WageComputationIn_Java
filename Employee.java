fc4d77692713753da0520fc0aed827641692069b
unknown <ravibs1994@gmail.com>
2020-10-24 11:49:00 +0530

    AddUC8_Emp_Wages_for_Multiple_Companies

--- Employee.java
+++ Employee.java
@@ -1,13 +1,11 @@
 package com.brigelabz;
 
+import java.util.Scanner;
+
 public class Employee {
 //define a method
-	public void CalculateEmpWages() {
+	public void CalculateEmpWages(int WAGES_PER_HOUR, int FULL_DAY_HOUR, int PART_TIME_DAY_HOUR, int DAYS_IN_MONTH) {
 //Local data Members
-		final int WAGES_PER_HOUR = 20;
-		final int FULL_DAY_HOUR = 8;
-		final int PART_TIME_DAY_HOUR = 4;
-		final int DAYS_IN_MONTH = 20;
 		int totalWages = 0;
 		int TotalWagesAsperCondition = 0;
 		int totalWorkingHour = 0;
@@ -33,12 +31,62 @@ public class Employee {
 			workingDays++;
 		}
 		System.out.println("Total Wages As per Given condition = " + TotalWagesAsperCondition);
-	}//CalculateEmpWages() method close
+	}// CalculateEmpWages() method close
 
 	public static void main(String[] args) {
 //object created
-		Employee emp = new Employee();
+		int empRatePerHour=0;
+		int fullDayHour=0;
+		int partDayHour=0;
+		int maxDays=0;
+		String choice = "";
+		Scanner sc = new Scanner(System.in);
 //method call
-		emp.CalculateEmpWages();
-	}//main() method close
-}//class close
+		do {
+			Employee emp = new Employee();
+			int random = (int) Math.floor(Math.random() * 10) % 3;
+			switch (random) {
+			case 0:
+				 System.out.println(" Infosys ");
+				 System.out.println("Enter empRatePer Hour ");
+				 empRatePerHour=sc.nextInt();
+				 System.out.println("Enter Full day Hour ");
+				 fullDayHour=sc.nextInt();
+				 System.out.println("Enter partime day Hour ");
+				 partDayHour=sc.nextInt();
+				 System.out.println("Enter MaxDays ");
+				 maxDays=sc.nextInt();
+				 emp.CalculateEmpWages(empRatePerHour,fullDayHour,partDayHour,maxDays);// WAGES_PER_HOUR,FULL_DAY_HOUR, PART_TIME_DAY_HOUR,DAYS_IN_MONTH
+				break;
+			case 1:
+				 System.out.println("TCS");
+				 System.out.println("Enter empRatePer Hour ");
+				 empRatePerHour=sc.nextInt();
+				 System.out.println("Enter Full day Hour ");
+				 fullDayHour=sc.nextInt();
+				 System.out.println("Enter partime day Hour ");
+				 partDayHour=sc.nextInt();
+				 System.out.println("Enter MaxDays ");
+				 maxDays=sc.nextInt();
+				 emp.CalculateEmpWages(empRatePerHour,fullDayHour,partDayHour,maxDays);// WAGES_PER_HOUR,FULL_DAY_HOUR, PART_TIME_DAY_HOUR,DAYS_IN_MONTH
+				break;
+			case 2:
+				  System.out.println("IBM");
+				  System.out.println("Enter empRatePer Hour ");
+				  empRatePerHour=sc.nextInt();
+				  System.out.println("Enter Full day Hour ");
+				  fullDayHour=sc.nextInt();
+				  System.out.println("Enter partime day Hour ");
+				  partDayHour=sc.nextInt();
+				  System.out.println("Enter MaxDays ");
+				  maxDays=sc.nextInt();
+				  emp.CalculateEmpWages(empRatePerHour,fullDayHour,partDayHour,maxDays);// WAGES_PER_HOUR,FULL_DAY_HOUR, PART_TIME_DAY_HOUR,DAYS_IN_MONTH
+				break;
+			}
+			System.out.println("Do you want to Continue Y/N");
+			choice = sc.next();
+		} while (choice.equalsIgnoreCase("Y"));
+		System.out.println("Thank you..");
+		sc.close();
+	}// main() method close
+}// class close
